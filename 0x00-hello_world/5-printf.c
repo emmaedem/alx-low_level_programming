0a55efbe896e101b70f0e53a56bddb805382ea52
emmaedem <emmaontop86@yahoo.com>
2022-12-08 18:40:46 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the main string
+ * Description: C program that prints exactly
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
