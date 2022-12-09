a043f1f950e0bdebdce79ef4bbce701ac8faad32
emmaedem <emmaontop86@yahoo.com>
2022-12-08 17:18:36 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Hello , puts
+ * description : i am writing my first c programme
+ * Return : Always 0
+ */
+
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+}
