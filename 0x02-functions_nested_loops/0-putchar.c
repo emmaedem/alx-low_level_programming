35452c6734b070bad03bb1b185660bd875dffcf5
emmaedem <emmaontop86@yahoo.com>
2022-12-13 15:43:39 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#include <main.h>
+
+/**
+ * main - main function to print _putchar and a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void jack_bauer(void);
+void times_table d times_table (void);
+void print_to_98(int n);
+
+#endif
