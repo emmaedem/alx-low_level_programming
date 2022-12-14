a1e3f27af2ef6464d705152e43e4e562e8c1849f
emmaedem <emmaontop86@yahoo.com>
2022-12-14 03:20:21 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,8 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 void jack_bauer(void);
-void times_table d times_table (void);
+void times_table(void);
 void print_to_98(int n);
+int _putchar(char c);
 
 #endif
