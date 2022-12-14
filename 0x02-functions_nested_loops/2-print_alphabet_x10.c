705cbdeca779e716a0850db7fccc9f068b663fb4
emmaedem <emmaontop86@yahoo.com>
2022-12-14 04:45:08 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,13 @@ void print_alphabet_x10(void)
 	int ten;
 	char la;
 
-	for (ten = 'a' ten <= 9; ten++)
+	for (ten = 'a'; ten <= 9 ten++;)
 	{
 		for (la = 'a'; la <= 'z'; la++)
+
 			_putchar(la);
 
-		_puncher('\n');
+		_putchar('\n');
 	}
 
 }
