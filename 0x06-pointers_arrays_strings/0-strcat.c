8167db5a46e19b3fa9f39177c35ac298cb127b4e
emmaedem <emmaontop86@yahoo.com>
2022-12-23 14:55:25 -0800

    main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _street -concatenates two strings
+ * @dest: input alue
+ * src: input alue
+ *
+ Return: void
+*/
+char *street(char *(dest, char *src )
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[1] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++'
+		j++;
+	}	
+	dest[i] = '\0';	
+	return (dest);
+}
