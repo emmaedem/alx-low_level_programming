0dc19534a2c534954f08cd11fb3109f16c382056
emmaedem <emmaontop86@yahoo.com>
2023-01-11 13:26:30 -0800

    1-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that print arugument
+ * @argc: 
+ * @argv: 
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[]);
+{
+	printf("%d\n", argc -1);
+
+	return 0;
+
+}
