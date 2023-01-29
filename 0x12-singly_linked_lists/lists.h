d9df1d1377f06e68b5ef16f94aa3c96cdc82f365
emmaedem <emmaontop86@yahoo.com>
2023-01-29 14:40:39 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,8 +7,7 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list
  */
 typedef struct list_s
 {
@@ -22,5 +21,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 
 #endif
