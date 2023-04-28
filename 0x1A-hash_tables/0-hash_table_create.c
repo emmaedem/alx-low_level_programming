831170b6253df78185d22daaf5e8678e806569d9
emmaedem <emmaontop86@yahoo.com>
2023-04-28 08:41:57 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 
-
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
@@ -8,7 +7,6 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new hash table.
  */
-
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
-
 /**
  * main - check the code
  *
@@ -13,10 +12,10 @@
 int main(void)
 {
 
-    hash_table_t *ht;
+hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    return (EXIT_SUCCESS);
+ht = hash_table_create(1024);
+hash_table_set(ht, "betty", "cool");
+return (EXIT_SUCCESS);
 
 }
